1805e5444eec49d2d2c79c21f4f4698223355f2b
MY <715711877@qq.com>
2022-04-11 18:01:10 +0800

    composing complex interfaces

--- Landmarks/Landmarks/Views/ContentView.swift
+++ Landmarks/Landmarks/Views/ContentView.swift
@@ -9,9 +9,26 @@ import SwiftUI
 import MapKit
 
 struct ContentView: View {
+    @State private var selection: Tab = .featured
+    
+    enum Tab {
+        case featured
+        case list
+    }
     
     var body: some View {
-        LandmarkList()
+        TabView(selection: $selection) {
+            CategoryHome()
+                .tabItem{
+                    Label("Featuredd", systemImage: "star")
+                }
+                .tag(Tab.featured)
+            LandmarkList()
+                .tabItem{
+                    Label("List", systemImage: "list.bullet")
+                }
+                .tag(Tab.list)
+        }
         
         
     }
